0628fd76aa3584aa0ef4691edf028fc282f89a91
9oodam <godamcity98@gmail.com>
2024-06-08 17:34:43 +0900

    feat: facebook api edit: con, service

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,44 @@
 import { Injectable } from '@nestjs/common';
+import FB from 'fb';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  accessToken: string;
+
+  constructor() {
+    this.accessToken = 'EAAb6aDIUTLcBO1rI4eb0P32ZAzB8CdMwums3NTPHKxZBMnhGNp7oDxzAd26zPsiekm6vlX8ZA2bRtIT0QZAz9Nd7xUSu86iGoOHW3UHM9ZAmPf123nrbHFsuZCggdChOIq2OK2aAZBLnRqmZC1GP8Y0F9l0HF1dtTuq9Ri3EL3ZCQE77JK17MSq8SEeOpnpX6eg0CWecZBxaLnX7lLqmEWXZCynKsGZC8xVg90rE'
+    FB.setAccessToken(this.accessToken);
+  }
+
+  getFeed(): Promise<any> {
+    return new Promise((resolve, reject) => {
+      FB.api(
+        '/325237764009663/feed',
+        'GET',
+        {},
+        function(response: any) {
+          if (response && !response.error) {
+            resolve(response.data);
+          } else {
+            reject(response.error);
+          }
+        }
+      );
+    });
+  }
+
+  getMsg(): Promise<any> {
+    return new Promise((resolve, reject) => {
+      FB.api(
+        "/325237764009663",
+        function (response: any) {
+          if (response && !response.error) {
+            resolve(response);
+          }else {
+            reject(response.error);
+          }
+        }
+    );
+    });
   }
 }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+
+const corsOptions: CorsOptions = {
+  origin: ['http://127.0.0.1:5500'], // 허용할 클라이언트 도메인
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // 허용할 HTTP 메서드
+  allowedHeaders: ['Content-Type', 'Authorization'], // 허용할 헤더
+};
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors(corsOptions);
+  await app.listen(8080); // 백엔드 서버 포트
 }
 bootstrap();
