3371749d2fcfab5ef79d1a0c44541369565f5126
9oodam <godamcity98@gmail.com>
2024-06-19 17:40:33 +0900

    feat: 채팅 웹소켓 기능 추가

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Query, Res } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('getFeed')
   getFeed(): any {
     return this.appService.getFeed();
   }
+
+  // @Post('sendChat')
+  // sendChat(): any {
+  //   return this.appService.sendChat();
+  // }
 }
--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -3,11 +3,11 @@ import FB from 'fb';
 
 @Injectable()
 export class AppService {
-  accessToken: string;
+  fbAccessToken: string;
 
   constructor() {
-    this.accessToken = 'EAAb6aDIUTLcBO1rI4eb0P32ZAzB8CdMwums3NTPHKxZBMnhGNp7oDxzAd26zPsiekm6vlX8ZA2bRtIT0QZAz9Nd7xUSu86iGoOHW3UHM9ZAmPf123nrbHFsuZCggdChOIq2OK2aAZBLnRqmZC1GP8Y0F9l0HF1dtTuq9Ri3EL3ZCQE77JK17MSq8SEeOpnpX6eg0CWecZBxaLnX7lLqmEWXZCynKsGZC8xVg90rE'
-    FB.setAccessToken(this.accessToken);
+    this.fbAccessToken = 'EAAb6aDIUTLcBO7cEXKO59HaXzRN4Yecw3taYszsxEkb9Ke42uVZChP7ZBTJXkTDidq7kZAFY7yQRCyH9mZCwCih0968K9VKRXtH3ZBH2TZCAlGqGMZC30DRjxMzavzXc4cQmxE91ghT1UhRYaQLNZBetMBJHLKm63Kz5DZCsOAzZBJ3PGZBUKZALiVNvHyzsa7cKwWcdLCxv3MK4'
+    FB.setAccessToken(this.fbAccessToken);
   }
 
   getFeed(): Promise<any> {
@@ -27,18 +27,38 @@ export class AppService {
     });
   }
 
-  getMsg(): Promise<any> {
-    return new Promise((resolve, reject) => {
-      FB.api(
-        "/325237764009663",
-        function (response: any) {
-          if (response && !response.error) {
-            resolve(response);
-          }else {
-            reject(response.error);
-          }
-        }
-    );
-    });
-  }
+  // getAlbum(): Promise<any> {
+  //   return new Promise((resolve, reject) => {
+  //     FB.api('325237764009663/albums', 'GET', {}, (response) => {
+  //       if (response && !response.error) {
+  //         resolve(response.data);
+  //       } else {
+  //         reject(response.error);
+  //       }
+  //     });
+  //   });
+  // }
+
+  // getPhoto(): any {
+  //   const album = this.getAlbum();
+  //   FB.api(
+  //     '/122104603616345521/picture',
+  //     'GET',
+  //     {},
+  //     function(response) {
+  //         // Insert your code here
+  //     }
+  //   );
+  // }
+
+
+  // 채팅 관련
+  // sendChat(): Promise<any> {
+
+  // }
 }
+
+// /accounts?fields=name,access_token&access_token=
+// /accounts?fields=about,attire,bio,location,parking,hours,emails,website&access_token=
+
+// PAGE-ID/insights?metric=page_messages_new_conversations_unique,page_messages_blocked_conversations_unique&access_token=PAGE-ACCESS-TOKEN"
